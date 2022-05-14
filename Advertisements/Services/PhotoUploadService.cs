9e29d197254c2f502ecf42944a9b2aee3b1e5e82
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-05-15 02:19:14 +0400

    Upload photo service using Cloudinary was added

---
+++ Advertisements/Services/PhotoUploadService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Advertisements.Services
+{
+    public class PhotoUploadService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoUploadService(IConfiguration config)
+        {
+            var acc = new Account
+            (
+                config["Cloudinary:CloudName"],
+                config["Cloudinary:ApiKey"],
+                config["Cloudinary:ApiSecret"]
+            );
+
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddImageAsync(IFormFile photo)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (photo.Length > 0)
+            {
+                using var stream = photo.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(photo.FileName, stream)
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeleteImageAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+
+            return result;
+        }
+    }
+}
