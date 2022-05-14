36fc5316f51914b9f76524cf37cf2fa176ff8241
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-05-15 02:32:37 +0400

    adding Auto Mapper

---
+++ Advertisements/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Advertisements.Dto;
+using Advertisements.Models;
+using AutoMapper;
+
+namespace Advertisements.Mapping
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateAdvertisementDto, Advertisement>().ReverseMap();
+
+        }
+    }
+}
